bcb867a07647823625295aa256275349de159d17
Ivy Lin <ivy.lin.f2e@gmail.com>
2020-06-14 22:25:56 +0800

    props ruleForm

--- helli-world/src/components/Search.vue
+++ helli-world/src/components/Search.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="search">
-    <Timetable />
     <el-row>
       <el-col :xs="12" :md="24">
         <div class="button-wrap">
@@ -12,17 +11,75 @@
             size="100%"
             style="margin-top: 60px"
           >
-            <el-table
-              :data="tableData"
-              highlight-current-row
-              @current-change="handleCurrentChange"
-              :default-sort="{prop: '!startTime', order: 'descending'}"
-            >
-              <el-table-column prop="train" label="車次" sortable min-width="30"></el-table-column>
-              <el-table-column prop="startTime" label="出發時間" sortable min-width="50"></el-table-column>
-              <el-table-column prop="endTime" label="到達時間" sortable min-width="50"></el-table-column>
-              <el-table-column prop="total" label="行車時間" sortable min-width="50"></el-table-column>
-            </el-table>
+            <el-form :model="ruleForm" :rules="rules" ref="ruleForm">
+              <el-row>
+                <el-col :xs="24" :sm="10" :md="5">
+                  <div>
+                    <el-form-item prop="from" required class="col bg-purple">
+                      <el-select v-model="ruleForm.from" placeholder="出發站">
+                        <el-option label="南港" value="0990"></el-option>
+                        <el-option label="台北" value="1000"></el-option>
+                        <el-option label="板橋" value="1010"></el-option>
+                        <el-option label="桃園" value="1020"></el-option>
+                        <el-option label="新竹" value="1030"></el-option>
+                        <el-option label="苗栗" value="1035"></el-option>
+                        <el-option label="台中" value="1040"></el-option>
+                        <el-option label="彰化" value="1043"></el-option>
+                        <el-option label="雲林" value="1047"></el-option>
+                        <el-option label="嘉義" value="1050"></el-option>
+                        <el-option label="台南" value="1060"></el-option>
+                        <el-option label="左營" value="1070"></el-option>
+                      </el-select>
+                    </el-form-item>
+                  </div>
+                </el-col>
+                <el-col :xs="24" :sm="10" :md="5">
+                  <div class="col bg-purple">
+                    <el-form-item prop="to" required>
+                      <el-select v-model="ruleForm.to" placeholder="到達站">
+                        <el-option label="南港" value="0990"></el-option>
+                        <el-option label="台北" value="1000"></el-option>
+                        <el-option label="板橋" value="1010"></el-option>
+                        <el-option label="桃園" value="1020"></el-option>
+                        <el-option label="新竹" value="1030"></el-option>
+                        <el-option label="苗栗" value="1035"></el-option>
+                        <el-option label="台中" value="1040"></el-option>
+                        <el-option label="彰化" value="1043"></el-option>
+                        <el-option label="雲林" value="1047"></el-option>
+                        <el-option label="嘉義" value="1050"></el-option>
+                        <el-option label="台南" value="1060"></el-option>
+                        <el-option label="左營" value="1070"></el-option>
+                      </el-select>
+                    </el-form-item>
+                  </div>
+                </el-col>
+                <el-col :xs="24" :sm="4" :md="2">
+                  <div class="col bg-purple-light">
+                    <el-button type="primary" @click="changeFromTo" icon="el-icon-refresh"></el-button>
+                  </div>
+                </el-col>
+                <el-col :xs="24" :sm="10" :md="5">
+                  <div class="col bg-purple">
+                    <el-form-item prop="time">
+                      <el-time-picker placeholder="出發時間" v-model="ruleForm.time"></el-time-picker>
+                    </el-form-item>
+                  </div>
+                </el-col>
+                <el-col :xs="24" :sm="10" :md="5">
+                  <div class="col bg-purple-light">
+                    <el-form-item prop="date">
+                      <el-date-picker type="date" placeholder="出發日期" v-model="ruleForm.date"></el-date-picker>
+                    </el-form-item>
+                  </div>
+                </el-col>
+                <el-col :xs="24" :sm="4" :md="2">
+                  <div class="col bg-purple">
+                    <el-button type="primary" @click="submitForm('ruleForm')" icon="el-icon-search"></el-button>
+                  </div>
+                </el-col>
+              </el-row>
+            </el-form>
+            <Timetable :ruleForm="ruleForm" @submit="submitForm" />
           </el-drawer>
         </div>
       </el-col>
@@ -93,46 +150,38 @@ export default {
       drawerfare: false,
       drawersaved: false,
       drawerticketing: false,
-      tableData: [
-        {
-          train: "803",
-          startTime: "06:26",
-          endTime: "08:40",
-          total: "2:14"
-        },
-        {
-          train: "203",
-          startTime: "06:30",
-          endTime: "08:15",
-          total: "1:45"
-        },
-        {
-          train: "603",
-          startTime: "06:51",
-          endTime: "08:50",
-          total: "1:59"
-        },
-        {
-          train: "805",
-          startTime: "07:11",
-          endTime: "09:25",
-          total: "2:14"
-        },
-        {
-          train: "109",
-          startTime: "07:31",
-          endTime: "09:05",
-          total: "1:34"
-        }
-      ]
+      ruleForm: {
+        from: "",
+        to: "",
+        time: "",
+        date: ""
+      },
+      rules: {
+        from: [{ required: true, message: " ", trigger: "change" }],
+        to: [{ required: true, message: " ", trigger: "change" }],
+        time: [
+          { type: "date", required: true, message: " ", trigger: "change" }
+        ],
+        date: [
+          { type: "date", required: true, message: " ", trigger: "change" }
+        ]
+      }
     };
   },
   mounted() {},
   computed: {},
   methods: {
-    handleCurrentChange(val) {
-      this.currentRow = val;
-      console.log(val);
+    changeFromTo() {},
+    submitForm(TimetableSearch) {
+      this.$refs[TimetableSearch].validate(valid => {
+        if (valid) {
+          alert("submit!");
+          // console.log(this.ruleForm);
+        } else {
+          console.log("error submit!!");
+          return false;
+        }
+      });
     }
   }
 };
@@ -146,10 +195,11 @@ export default {
   min-width: 100%;
   padding: 30px 0;
 }
-.bg-purple {
-  background: #d3dce6;
-}
-.bg-purple-light {
-  background: #e5e9f2;
+.col {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 5px 0;
 }
 </style>
